3ba8f1c2b38c2349b59de12261010b9745521a11
Rafael <rafaelbthomaz@gmail.com>
2025-02-07 15:00:38 -0300

    adding totals

--- scripts.js
+++ scripts.js
@@ -4,7 +4,9 @@ const expense = document.getElementById("expense")
 const category = document.getElementById("category")
 
 const expenseList = document.querySelector("ul")
-const expenseQuantity = document.querySelector("aside header p span")
+const expensesTotal = document.querySelector("aside header h2")
+const expensesQuantity = document.querySelector("aside header p span")
+
 
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "")
@@ -82,7 +84,32 @@ function updateTotals(){
   try {
     const items = expenseList.children
 
-    expenseQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+    expensesQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+
+    let total = 0 
+
+    for(let item = 0; item < items.length; item++){
+      const itemAmount = items[item].querySelector(".expense-amount")
+
+      let value = itemAmount.textContent.replace(/[^\d,]/g, "").replace(",",".")
+
+      value = parseFloat(value)
+
+      if (isNaN(value)){
+        return alert("Não foi possível calcular o total. O valor não parece ser um número.")
+      }
+
+      total += Number(value)
+    }
+
+    const symbolBRL = document.createElement("small")
+    symbolBRL.textContent = "R$"
+
+    total = fomartCurrencyBRL(total).toUpperCase().replace("R$", "")
+
+    expensesTotal.innerHTML = ""
+
+    expensesTotal.append(symbolBRL, total)
 
   } catch (error) {
     console.log(error)
