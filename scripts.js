cdf3c33f13590542eecaa5dfa23cff0c8bb7e7dc
Rafael <rafaelbthomaz@gmail.com>
2025-02-07 15:36:43 -0300

    adding clear form and focus

--- scripts.js
+++ scripts.js
@@ -72,6 +72,8 @@ function expenseAdd(newExpense){
     expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
     expenseList.append(expenseItem)
 
+    formClear()
+
     updateTotals()
 
   } catch (error) {
@@ -124,6 +126,14 @@ expenseList.addEventListener("click", function (event) {
   }
   updateTotals()
 })
+
+function formClear(){
+  expense.value = ""
+  category.value = ""
+  amount.value = ""
+
+  expense.focus()
+}
 
 
 
